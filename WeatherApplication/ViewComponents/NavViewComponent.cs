d05fb74f93c333d08697afdc69ad3560e482481b
Ahmed Montasser <ahmed.sharkawy99@hotmail.com>
2023-12-23 14:48:45 +0200

    Adding View Component

---
+++ WeatherApplication/ViewComponents/NavViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApplication.ViewComponents
+{
+    [ViewComponent]
+    public class NavViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string name)
+        {
+            return View(model:name);
+        }
+    }
+}
